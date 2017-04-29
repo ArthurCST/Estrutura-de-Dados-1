aca1e45151bdc4f482f30338a1987f18cf313bef
Arthur Serra <arthur.serra10@gmail.com>
2017-04-29 15:58:28 -0300

    Update pilha.c

--- pilha.c
+++ pilha.c
@@ -1,103 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct Pilha
+typedef struct _PILHA_                              /*Estrutura da Pilha*/
 {
-    int max, size, beg, end;
-    int* vet;
+    int max, size, beg, end;                        /*Caracteristicas*/
+    int* vet;                                       /*Armazenamento de dados*/
 
-}pilha;
+}PILHA;
 
-pilha* criar_pilha(int max);
-void inserir_pilha(pilha* aux, int x);
-void remover_pilha(pilha* aux);
-void destruir_pilha(pilha* aux);
-int mostrar_pilha(pilha* aux, int x);
+PILHA* criar_pilha(int max);                        /*Cria e retorna Pilha*/
+void inserir_pilha(PILHA* pilha, int num);          /*Empilha elemento (push)*/
+void remover_pilha(PILHA* pilha);                   /*Desempilha elemento (pop)*/
+void destruir_pilha(PILHA* pilha);
+int mostrar_pilha(PILHA* pilha, int num);           /*Retorna elemento da posição num sem dar pop (stackpop)*/
 
-void main(void)
+
+int main()
 {
-    pilha *p;
-    char *aux;
-    int a;
+    PILHA *p;
+    int a, i;
 
     p = criar_pilha(3);
     inserir_pilha(p, 1);
     inserir_pilha(p, 2);
     inserir_pilha(p, 3);
-    a = mostrar_pilha(p, 0);
-    printf("%d", a);
+    remover_pilha(p);
+    inserir_pilha(p, 4);
+    for(i = p->size-1; i >=0 ; i--)
+    {
+        a = mostrar_pilha(p, i);
+        printf("%d\n", a);
+    }
 
-
+    return 0;
 }
 
-pilha* criar_pilha(int max)
+PILHA* criar_pilha(int max)
 {
-    pilha* aux;
-
-    if(max > 0)
+    if(max > 0)                                                 /*Testa se o espaco maximo de armazenamento eh vaildo*/
     {
-        aux = (pilha*)malloc(sizeof(pilha));
-        if(aux != NULL)
-        {
-            aux->vet = (int*)malloc(max*sizeof(int));
-            if(aux->vet != NULL)
-            {
-                aux->max = max;
-                aux->size = 0;
-                aux->beg = 0;
-                aux->end = max;
+        PILHA* pilha = (PILHA*)malloc(sizeof(PILHA));           /*Aloca memoria para armazenar a estrutuda PILHA*/
 
-                return aux;
-            }
+        pilha->max = max;                                       /*Atributo max da pilha recebe o valor informado*/
+        pilha->vet = (int*)malloc(sizeof(int)* pilha->max);     /*Vetor de armazenamento aloca o espaco max para pilha*/
+        pilha->beg = 0;                                         /*Inicializa topo da pilha*/
+        pilha->size = 0;                                        /*Inicializa espaco ocupado da pilha*/
+        pilha->end = pilha->max;                                /*Atributo end recebe o limite de empilhamento da pilha*/
+
+        return pilha;                                           /*Retorna pilha criada e inicializada*/
+    }
+
+    return NULL;                                                /*Retorna NULL para falha na criação*/
+}
+
+void inserir_pilha(PILHA* pilha, int num)
+{
+    if((pilha != NULL) && (pilha->vet != NULL) && (pilha->size < pilha->max))   /*Verifica se a pilha e o vetor foram alocados e se a quantidade de elementos eh melhor q o tamnaho maximo*/
+    {
+        pilha->vet[pilha->size] = num;                                          /*insere elemento no topo da pilha*/
+        pilha->size++;                                                          /*almenta o tamnho da pilha*/
+
+        if(pilha->end < pilha->size -1)
+        {
+            pilha->end++;
+        }
+        else
+        {
+            pilha->end = 0;
         }
     }
-    return NULL;
 }
 
-void inserir_pilha(pilha* aux, int x)
+void remover_pilha(PILHA* pilha)
 {
-    if(aux != NULL && aux->vet != NULL && aux->size > aux->max)
+    if((pilha != NULL) && (pilha->vet != NULL) && (pilha->size >= 0))   /*Verifica se a pilha e o vetor foram alocados e se ha elementos na pilha*/
     {
-        aux->vet[aux->size] = x;
-        aux->size++;
+        pilha->size--;                                                  /*Decrementa tamnho pilha*/
 
-        if(aux->end < aux->size -1)
-            aux->end++;
+        if(pilha->beg < pilha->size -1)
+        {
+            pilha->beg++;
+        }
         else
-            aux->end = 0;
+        {
+            pilha->beg = 0;
+        }
     }
 }
 
-void remover_pilha(pilha* aux)
+void destruir_pilha(PILHA* pilha)
 {
-    int i;
-
-    if(aux != NULL && aux->vet != NULL && aux->size >= 0)
+    if(pilha != NULL && pilha->size < 0)    /*Verifica se a pilha existe e se ha elemento sa pilha*/
     {
-        aux->size--;
-
-        if(aux->beg < aux->size -1)
-            aux->beg++;
-        else
-            aux->beg = 0;
+        free(pilha->vet);                   /*Desaloca vetor de elementos*/
+        free(pilha);                        /*Desaloca pilha*/
     }
 }
 
-void destruir_pilha(pilha* aux)
+int mostrar_pilha(PILHA* pilha, int num)
 {
-    if(aux != NULL && aux->size <= 0)
+    if(pilha != NULL && pilha->vet != NULL && pilha->size >= num)   /*Verifica se a pilha e o vetor existem e se a posicao solicitada eh valida*/
     {
-        free(aux->vet);
-        free(aux);
-    }
-}
-
-int mostrar_pilha(pilha* aux, int x)
-{
-    if(aux != NULL && aux->vet != NULL)
-    {
-        return aux->vet[x];
+        return pilha->vet[num];                                     /*Retorna elemento da pilha*/
     }
     return 0;
 }
+
